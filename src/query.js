ad6df4e1b16747c4b062794c375cb15a29aa5af1
Jaco Greeff <jaco@parity.io>
2017-09-27 16:52:29 +0200

    Passing in accounts to isHandler

--- src/query.js
+++ src/query.js
@@ -14,7 +14,7 @@
 // You should have received a copy of the GNU General Public License
 // along with Parity.  If not, see <http://www.gnu.org/licenses/>.
 
-export default function isHandler (payload, account) {
+export default function isHandler (payload, accounts) {
   // Always handle all payloads
   return true;
 }
